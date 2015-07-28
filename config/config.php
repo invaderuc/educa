3d7e01a4e760a2db943dccb6ad9f1ca0c1194e56
Gerard <gerarduc@gmail.com>
2015-07-27 23:15:24 -0400

    4
    Cambios en view/ramo y nueva funcion config

--- config/config.php
+++ config/config.php
@@ -15,4 +15,17 @@
         $resultado2->close();
 	   return $result;
 	}
+
+	function apellido_persona($id) {
+		$db = mysqli_connect('localhost', 'root', '', 'educa');
+		mysqli_real_query($db,"SET NAMES 'utf8'");
+		ini_set('default_charset', 'UTF-8');
+	   $resultado2 = $db->query("SELECT * FROM persona WHERE PER_RUT = '$id'");
+
+        while ($fila2 = $resultado2->fetch_array()) {
+            $result=$fila2['PER_APELLIDO'];
+        }
+        $resultado2->close();
+	   return $result;
+	}
 ?>
